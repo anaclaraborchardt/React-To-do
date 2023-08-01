3aeaec8a9a83d9f99432c3274e63e63ad389e454
ANA CLARA TOMASELLI BORCHARDT <ana_c_borchardt@estudante.sesisenai.org.br>
2023-08-01 16:52:29 -0300

    forms css

--- src/form/Formulario.jsx
+++ src/form/Formulario.jsx
@@ -16,18 +16,18 @@ const Formulario = () => {
                     </label>
                     </div>
                 </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="flex">
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Digite o seu Nome
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
                         </input>
                     </label>
                     </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
+        
+              
                     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Digite o número do cadastro
-                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
+                        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
                         </input>
                     </label>
                     </div>
