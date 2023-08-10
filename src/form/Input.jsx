c0fb77e693c0e71ee900c3deb07d17ac92e70590
anaborchardt <anaclaratomaselliborchardt75@gmail.com>
2023-08-09 21:57:30 -0300

    onBlur, validation

--- src/form/Input.jsx
+++ src/form/Input.jsx
@@ -1,49 +1,69 @@
+import { useEffect } from "react";
 import {useState, useRef} from "react"
 import './Formulario.css'
 
 const Input = (cards) => {
-    const nameInputRef = useRef()
+    const passwordInputRef = useRef();
     const numberInputRef = useRef();
-    const [nameEnteredInput, setNameInput] = useState('')
-    const [numberEnteredInput, setNumberInput] = useState('')
-    const [validnameEnteredInput, setvalidNameInput] = useState(true)
-    const [validnumberEnteredInput, setvalidNumberInput] = useState(true)
-    const nameInputClasses = validnameEnteredInput ? 'valido' : 'invalido'
+    const [passwordEnteredInput, setPasswordInput] = useState('');
+    const [numberEnteredInput, setNumberInput] = useState('');
+    const [validpasswordEnteredInput, setvalidPasswordInput] = useState(true);
+    const [validnumberEnteredInput, setvalidNumberInput] = useState(false);
+    //inicialmente esse input não é acionado, por isso é false
+    const [numberTouched, setNumberTouched] = useState(false);
+
 
     const inputChangeHandler = (event) => {
-        setNameInput(event.target.value);
+        setPasswordInput(event.target.value);
     }
 
     const inputChangeHandlerNumber = (event) => {
         setNumberInput(event.target.value);
     }
 
+    const numberBlurHandler = event => {
+        setNumberTouched(true);
+
+        if(numberEnteredInput.trim() === ''){
+            setvalidNumberInput(false)
+            return;    
+        }
+    }
+
+    useEffect(() => {
+        if(validnumberEnteredInput){
+            console.log("cadastro válido")
+        }
+    }, [validnumberEnteredInput])
+
     const submitChangeHandler = (event) => {
         event.preventDefault();
 
-        if(nameEnteredInput.trim() === ''){
-            setvalidNameInput(false)
+        setNumberTouched(true);
+
+        if(passwordEnteredInput.trim() === ''){
+            setvalidPasswordInput(false)
             return;    
         }else if(numberEnteredInput.trim() === ''){
             setvalidNumberInput(false)
             return; 
         }
-        setvalidNameInput(true);
+        setvalidPasswordInput(true);
         setvalidNumberInput(true);
 
-        const enteredNameValue = nameInputRef.current.value
+        const enteredPasswordValue = passwordInputRef.current.value
         const enteredNumberValue = numberInputRef.current.value
      
 
         const dadosUsuarioCadastrado = {
-            nameEnteredInput: nameEnteredInput,
+            passwordEnteredInput: passwordEnteredInput,
             numberEnteredInput: numberEnteredInput
         }
 
         localStorage.setItem('dadosUsuarioCadastrado', JSON.stringify(dadosUsuarioCadastrado))
         console.log(dadosUsuarioCadastrado)
 
-        setNameInput('');
+        setPasswordInput('');
         setNumberInput('');
 
         
@@ -53,31 +73,35 @@ const Input = (cards) => {
         <div className="w-full max-w-xs, divForms" >
             <form  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
                  onSubmit = {submitChangeHandler}>
-                 <div className="mb-4">
+
+                    <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" 
-                    htmlFor="name">Qual o seu nome?
-                    <input type="text" 
-                    onChange={inputChangeHandler}
-                    ref={nameInputRef}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
-                    leading-tight focus:outline-none focus:shadow-outline" 
-                    placeholder="Nome"
-                    value={nameEnteredInput}></input>
-                    </label>
-                    {!validnameEnteredInput && <p>Você deve preencher seu nome</p>}
-                </div>
-                 <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" 
-                    htmlFor="name">Qual o número do cadastro ?
+                    htmlFor="name">Número do cadastro
                     <input type="number" 
                     onChange={inputChangeHandlerNumber}
+                    onBlur={numberBlurHandler}
                     ref={numberInputRef}
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
                     leading-tight focus:outline-none focus:shadow-outline" 
                     placeholder="Cadastro"
                     value={numberEnteredInput}></input>
-                </label>
-                {!validnumberEnteredInput && <p>Você deve preencher o número do cadastro</p>}
+                    </label>
+                    {!validnumberEnteredInput && <p>Você deve preencher seu nome</p>}
+                </div>
+
+                    
+                 <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" 
+                    htmlFor="name">Qual sua senha?
+                    <input type="text" 
+                    onChange={inputChangeHandler}
+                    ref={passwordInputRef}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+                    leading-tight focus:outline-none focus:shadow-outline" 
+                    placeholder="Senha"
+                    value={passwordEnteredInput}></input>
+                    </label>
+                    {!validpasswordEnteredInput && <p>Você deve preencher sua senha</p>}
                 </div>
                 
                 <button className="bg-blue-500 hover:bg-blue-700 text-white 
